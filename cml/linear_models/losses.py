76a7cf28fd1f4cf3ac29e06ed6ae7b62c4ba0e82
Matvey26 <matvey.ivchenko@yandex.ru>
2024-08-20 14:51:48 +0300

    Добавлена функция потерь Хьюбера

--- cml/linear_models/losses.py
+++ cml/linear_models/losses.py
@@ -71,6 +71,16 @@ class BaseLoss:
 
 
 class MSELoss(BaseLoss):
+    """
+    Класс для функции потерь MSE с опциональным регуляризатором
+
+    Параметры
+    ---------
+    regularizer : BaseRegularizer, опционально
+        Объект регуляризатора, который будет использоваться в расчете функции потерь. 
+        По умолчанию используется экземпляр BaseRegularizer с коэффициентом регуляризации 0.
+    """
+
     def calc_loss(self, X: np.ndarray, y: np.ndarray, w: np.ndarray):
         Q = np.mean((np.dot(X, w) - y) ** 2)
         R = self.regularizer.calc_reg(w)
@@ -86,6 +96,16 @@ class MSELoss(BaseLoss):
 
 
 class MAELoss(BaseLoss):
+    """
+    Класс для функции потерь MAE с опциональным регуляризатором
+
+    Параметры
+    ---------
+    regularizer : BaseRegularizer, опционально
+        Объект регуляризатора, который будет использоваться в расчете функции потерь. 
+        По умолчанию используется экземпляр BaseRegularizer с коэффициентом регуляризации 0.
+    """
+
     def calc_loss(self, X: np.ndarray, y: np.ndarray, w: np.ndarray):
         Q = np.mean(np.abs(np.dot(X, w) - y))
         R = self.regularizer.calc_reg(w)
@@ -95,6 +115,48 @@ class MAELoss(BaseLoss):
     def calc_grad(self, X: np.ndarray, y: np.ndarray, w: np.ndarray):
         err = np.dot(X, w) - y
         Q = np.dot(X.T, np.sign(err)) / len(y)
+        R = self.regularizer.calc_grad(w)
+
+        return Q + R
+
+
+class HuberLoss(BaseLoss):
+    """
+    Класс для функции потерь Huber с коэффициентом дельта и опциональным регуляризатором
+
+    Параметры
+    ---------
+    delta : float
+        Коэффициент функции потерь Хьюбера.
+    regularizer : BaseRegularizer, опционально
+        Объект регуляризатора, который будет использоваться в расчете функции потерь. 
+        По умолчанию используется экземпляр BaseRegularizer с коэффициентом регуляризации 0.
+    """
+
+    def __init__(self, delta: float, regularizer: BaseRegularizer = BaseRegularizer(0)):
+        super().__init__(regularizer)
+        self.delta = delta
+
+    def calc_loss(self, X: np.ndarray, y: np.ndarray, w: np.ndarray) -> np.float64:
+        z = np.dot(X, w) - y
+        phi = np.where(
+            np.abs(z) < self.delta,
+            0.5 * z ** 2,
+            self.delta * (np.abs(z) - 0.5 * self.delta)
+        )
+        Q = phi.mean()
+        R = self.regularizer.calc_reg(w)
+
+        return Q + R
+
+    def calc_grad(self, X: np.ndarray, y: np.ndarray, w: np.ndarray) -> np.ndarray:
+        z = np.dot(X, w) - y
+        phi = np.where(
+            np.abs(z) < self.delta,
+            z,
+            self.delta * np.sign(z)
+        )
+        Q = np.dot(X.T, phi) / len(y)
         R = self.regularizer.calc_grad(w)
 
         return Q + R
