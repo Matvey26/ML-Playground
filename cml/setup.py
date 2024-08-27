7021b4df4b3744f41566f3de8ef6d06a6396ad85
Matvey26 <matvey.ivchenko@yandex.ru>
2024-08-27 11:18:14 +0300

    Первая минимально рабочая версия приложения

--- cml/setup.py
+++ cml/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='cml',
-    packages=find_packages(include=['cml']),
+    packages=find_packages(exclude=['tests']),
     version='0.1.0',
     description='Библиотека для того, чтобы потыкать классическое машинное обучение.'
 )
